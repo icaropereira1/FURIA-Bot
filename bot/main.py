16c7bbbb121cd7d5af4da5fa4bf6606dd7867f50
Icaro Pereira Alves <xicaroestudos@gmail.com>
2025-04-29 14:42:48 -0300

    feat: registrando comando de modo bate papo

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,7 @@ import telebot
 
 bot = telebot.TeleBot('8160505498:AAEwwFnFz-UlNYlmKhU_rX7_Zzoyz03iHH4')
 
-from bot.handlers import start, loja, lineup, proximosjogos, titulos
+from bot.handlers import start, loja, lineup, proximosjogos, titulos, batepapo
 
 # Registra handlers
 start.register(bot)
@@ -10,6 +10,7 @@ loja.register(bot)
 lineup.register(bot)
 proximosjogos.register(bot)
 titulos.register(bot)
+batepapo.register(bot)
 
 print("Bot iniciado...")
 bot.polling()
