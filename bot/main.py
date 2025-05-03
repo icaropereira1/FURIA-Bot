55b45a82c2c97e1b96f6c0320b167bd36f0a2867
Icaro Pereira Alves <xicaroestudos@gmail.com>
2025-05-03 18:18:17 -0300

    feat: configurando modo de monitoramento

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,5 @@
-import os
-import telebot
-from dotenv import load_dotenv
-
-load_dotenv()
-
-bot = telebot.TeleBot(os.getenv('telegrambot_key'))
-
-from bot.handlers import start, loja, lineup, proximosjogos, titulos, batepapo, ativar_des_notificações
+from bot.core.bot_instance import bot
+from bot.handlers import start, loja, lineup, proximosjogos, titulos, batepapo, ativar_des_notificações, resultados
 from bot.services.monitoramento import iniciar_monitoramento
 
 iniciar_monitoramento(bot)
@@ -18,6 +11,7 @@ lineup.register(bot)
 proximosjogos.register(bot)
 titulos.register(bot)
 ativar_des_notificações.register(bot)
+resultados.register(bot)
 
 print("Bot iniciado...")
 bot.polling()
