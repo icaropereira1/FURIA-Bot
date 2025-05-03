c9a82710b3aa5d54f142ab778382e19d6be4fe91
Icaro Pereira Alves <xicaroestudos@gmail.com>
2025-05-03 18:21:28 -0300

    fix: modificando para funcionar com api pandascore

--- bot/handlers/proximosjogos.py
+++ bot/handlers/proximosjogos.py
@@ -1,11 +1,10 @@
-from bot.services.hltv import get_proximos_jogos
+from bot.services.pandascore import get_proximos_jogos
+
 
 def register(bot):
     @bot.message_handler(commands=['proximosjogos'])
     def handle_proximos_jogos(message):
-        jogos = get_proximos_jogos()
-
-        resposta = "🎮 **Próximos Jogos da FURIA (CS2):**\n\n"
-        resposta += "\n".join(f"- {jogo}" for jogo in jogos)
-
+        resposta = get_proximos_jogos()
         bot.send_message(message.chat.id, resposta, parse_mode="Markdown")
+
+        
