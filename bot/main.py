0adb535c7443094c8fbb0eac3df880f8f37392de
Icaro Pereira Alves <xicaroestudos@gmail.com>
2025-05-04 17:18:14 -0300

    feat: cadastrando novas comandos ao bot

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,5 @@
 from bot.core.bot_instance import bot
-from bot.handlers import ativar_desativar_notificações, start, loja, lineup, proximosjogos, titulos, batepapo, resultados
+from bot.handlers import ativar_desativar_notificações, start, loja, lineup, proximosjogos, titulos, batepapo, resultados, resultadosdetalhados, torcida
 from bot.services.monitoramento import iniciar_monitoramento
 
 iniciar_monitoramento(bot)
@@ -12,6 +12,8 @@ proximosjogos.register(bot)
 titulos.register(bot)
 ativar_desativar_notificações.register(bot)
 resultados.register(bot)
+resultadosdetalhados.register(bot)
+torcida.register(bot)
 
 print("Bot iniciado...")
 bot.polling()
