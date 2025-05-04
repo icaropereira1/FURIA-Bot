ca40ec7e445a8430fc1d31411fa1d4383642679b
Icaro Pereira Alves <xicaroestudos@gmail.com>
2025-05-04 17:09:24 -0300

    feat: adicionando função get_resultado_detalhados

--- bot/handlers/resultados.py
+++ bot/handlers/resultados.py
@@ -1,9 +1,9 @@
-from bot.core.bot_instance import bot
-from telebot.types import Message
-from bot.services.pandascore import get_resultados
+from bot.services.pandascore import get_resultados, match_ids_por_chat
 
 def register(bot):
-    @bot.message_handler(commands=["resultados"])
-    def handle_proximos_jogos(message):
-        resposta = get_resultados()
-        bot.send_message(message.chat.id, resposta, parse_mode="Markdown")
+    @bot.message_handler(commands=['resultados'])
+    def handle_resultados(message):
+        chat_id = message.chat.id
+        resposta, match_ids = get_resultados()  # Desempacota a tupla
+        match_ids_por_chat[chat_id] = match_ids  # Armazena para uso futuro
+        bot.reply_to(message, resposta, parse_mode="HTML")
