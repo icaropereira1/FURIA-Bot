099407327b6105f55ff702d89e437655ad15e558
Icaro Pereira Alves <xicaroestudos@gmail.com>
2025-05-03 23:40:24 -0300

    feat: atualizando comando proximos jogos

--- bot/handlers/proximosjogos.py
+++ bot/handlers/proximosjogos.py
@@ -4,7 +4,18 @@ from bot.services.pandascore import get_proximos_jogos
 def register(bot):
     @bot.message_handler(commands=['proximosjogos'])
     def handle_proximos_jogos(message):
-        resposta = get_proximos_jogos()
-        bot.send_message(message.chat.id, resposta, parse_mode="Markdown")
+        jogos = get_proximos_jogos()
 
-        
+        if not jogos:
+            texto = "❌ Nenhum jogo encontrado."
+        else:
+            texto = "*🔥Próximos jogos da FURIA:🔥*\n\n"
+            for jogo in jogos:
+                texto += f"🏆 *Campeonato: *{jogo['campeonato']}\n"
+                texto += f"🆚 {jogo['oponente']}\n"
+                texto += f"📅 {jogo['data']} às {jogo['hora']}\n"
+                if jogo["link"]:
+                    texto += f"🔴 [Assistir ao vivo]({jogo['link']})\n"
+                texto += "──────────────\n"
+
+        bot.send_message(message.chat.id, texto, parse_mode="Markdown")
